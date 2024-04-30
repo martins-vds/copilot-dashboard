59801805521bd2a2577b7917cce62eb639a00b99
Vinny Martins <martins.vds@outlook.com>
2024-04-30 23:03:27 +0000

    Update dependencies and add @octokit/oauth-app to api/package.json, api/tsconfig.json, and api/src/github.ts

--- api/src/github.ts
+++ api/src/github.ts
@@ -1,19 +1,18 @@
-import { OAuthApp, Octokit } from "octokit";
+import { Octokit } from "octokit";
 import { github_config } from "./config";
 
 export const createToken = async (code: string) => {
+    const OAuthApp = await import("@octokit/oauth-app");
 
-    const app = new OAuthApp({
+    const app = new OAuthApp.OAuthApp({
+        clientType: "oauth-app",
         clientId: github_config.client_id,
         clientSecret: github_config.client_secret,
     });
 
     const {
-        authentication: { token }
-    } = await app.createToken({
-        code        
-    });
-
+        authentication: { token },
+    } = await app.createToken({ code });
     return token;
 }
 
