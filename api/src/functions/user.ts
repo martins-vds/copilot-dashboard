4f9a584c0239ac3dee570884be95e447aaee1f96
Vinny Martins <martins.vds@outlook.com>
2024-05-01 20:16:26 +0000

    Update error handling in API functions and add token parameter to context.error in user.ts

--- api/src/functions/user.ts
+++ api/src/functions/user.ts
@@ -10,7 +10,7 @@ export async function user(request: HttpRequest, context: InvocationContext): Pr
 
         return { body: JSON.stringify(user), headers: { 'Content-Type': 'application/json' } };
     } catch (error) {
-        context.error(`Error getting user`, error);
+        context.error(`Error getting user`, error, token);
         return { status: error.status, body: JSON.stringify({ message: error.message }), headers: { 'Content-Type': 'application/json' } };
     }
 };
